223f8065f0dec5c933147e09f7c894715ae4c827
victortulabot <44962471+victortulabot@users.noreply.github.com>
2020-04-04 16:10:16 +0800

    express

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const handlebars = require('handlebars');
+const path = require('path');
 const app = express();
 const port = 9090;
 
@@ -11,3 +12,15 @@ app.set('view engine', 'hbs');
 app.listen(port, function(){
     console.log('App listening at port ' + port)
 })
+
+app.get('/', function (req, res){
+    res.sendFile(__dirname + '/login.html')
+});
+
+app.get('/login', function (req, res){
+    res.sendFile(__dirname + '/login.html')
+});
+
+app.get('/timeline', function (req, res){
+    res.sendFile(__dirname + '/timeline.html')
+});
