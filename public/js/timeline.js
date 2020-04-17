7ea76d982554472fd90d7a5e7fa6dc31d0fba396
iravillanuevaa <ira_villanueva@dlsu.edu.ph>
2020-04-17 22:28:53 +0800

    added js functions for upvote and downvote

--- public/js/timeline.js
+++ public/js/timeline.js
@@ -4,5 +4,31 @@ $(document).ready(function () {
     
     //$('#error').text('User does not exist!');
     $('#sideprofile').load('/getSideProfile');
-    
+
+
+    $(".upvotee").click(function() {
+        if ($(this).attr("class") == "upvotee"){
+            // if ($(this).closest('img').attr("src") === '/img/downvoted.png') {
+            //     $(this).next().attr('src', '/img/downvote.png');
+            //     this.src = this.src.replace("/img/upvote.png","/img/upvoted.png");
+            // }
+            this.src = this.src.replace("/img/upvote.png","/img/upvoted.png");
+        } 
+        else{
+            this.src = this.src.replace("/img/upvoted.png","/img/upvote.png");
+        }
+
+        $(this).toggleClass("on");
+    });
+
+    $(".downvotee").click(function() {
+        if ($(this).attr("class") == "downvotee"){
+            this.src = this.src.replace("/img/downvote.png","/img/downvoted.png");
+        } 
+        else{
+            this.src = this.src.replace("/img/downvoted.png","/img/downvote.png");
+        }
+        $(this).toggleClass("on");
+    });
+
 })
