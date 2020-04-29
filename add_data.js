36c13472ecac5db54641a3ca1c0b9b6c717495d3
victortulabot <victor_tulabot@dlsu.edu.ph>
2020-04-30 00:29:59 +0800

    Update add_data.js

--- add_data.js
+++ add_data.js
@@ -7,7 +7,23 @@ const userPost = 'userPost';
 
 
 // call function createDatabase 
-db.createDatabase();
+// db.createDatabase();
+
+// Atlas
+const MongoClient = require('mongodb').MongoClient;
+
+// replace the uri string with your connection string.
+const uri = "mongodb+srv://blycuasi:lasalle@cluster0-hinlh.mongodb.net/test?retryWrites=true&w=majority";
+MongoClient.connect(uri, function(err, client) {
+   if(err) {
+        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+   }
+   console.log('Connected...');
+   const collection = client.db("test").collection("devices");
+   // perform actions on the collection object
+   client.close();
+});
+// End of Atlas
 
 // create/populate userProfile objects containing fields such as Username, Email, Password, DisplayPicture, CreditScore, SavedPostID, University, Bio
 var user = {
