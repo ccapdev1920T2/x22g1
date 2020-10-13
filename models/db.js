7b7857c8a1d59fb9ee06a5e117d1f1abba9c670a
iravillanuevaa <ira_villanueva@dlsu.edu.ph>
2020-10-13 11:43:26 +0800

    updated db.js

--- models/db.js
+++ models/db.js
@@ -4,7 +4,7 @@ if(process.env.PORT == null || process.env.PORT == "") {
     const url = 'mongodb://localhost:27017/Big4FW_database';
 } else {
     const MongoClient = require('mongodb').MongoClient;
-    const url = "mongodb+srv://admin:iamnotsosecure12345@cluster0.nwynq.mongodb.net/Big4FW?retryWrites=true&w=majority";
+    const url = "mongodb://victor:lasalle@cluster0-shard-00-00-fxgom.mongodb.net:27017,cluster0-shard-00-01-fxgom.mongodb.net:27017,cluster0-shard-00-02-fxgom.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
 
     MongoClient.connect(url, function(err, client){
         if(err) {
