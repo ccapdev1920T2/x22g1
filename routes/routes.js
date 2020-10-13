c05e8333b60e88559cc026d5acb416033a0e87ea
iravillanuevaa <ira_villanueva@dlsu.edu.ph>
2020-10-13 12:13:33 +0800

    updated routes, controllers + added avatars folder

--- routes/routes.js
+++ routes/routes.js
@@ -1,76 +1,103 @@
-// import module `express`
-const express = require('express');
+// IMPORT
+const express = require('express'); //EXPRESS
+const session = require('express-session'); //EXPRESS-SESSIONS
+const database = require('../models/db.js'); //CONNECT DB
 
-// import module `controller` from `../controllers/controller.js`
-const controller = require('../controller/controller.js');
+const cookieParser = require('cookie-parser'); //COOKIES
+const bodyParser = require('body-parser'); //BODY PARSING
+var multer = require('multer'); //FILE UPLOAD
 
-const signUpController = require('../controller/signUpController.js');
-const timelineController = require('../controller/timelineController.js');
-const indivpostController = require('../controller/indivpostController.js');
-const createPostController = require('../controller/createPostController.js');
-const profileController = require('../controller/profileController.js');
+// import login controller
+const loginController = require('../controllers/loginController.js');
+
+// import sign up controller
+const signUpController = require('../controllers/signUpController.js');
+
+// import timeline controller
+const timelineController = require('../controllers/timelineController.js');
+const indivpostController = require('../controllers/indivpostController.js');
+const createPostController = require('../controllers/createPostController.js');
+
+// import profile controller
+const profileController = require('../controllers/profileController.js');
 
 const app = express();
 
-app.get('/favicon.ico', controller.getFavicon);
+// initialize multer for file upload use 
+// var storage = multer.diskStorage({
+//     destination: function (req, file, cd) {
+//         if (file.fieldname === 'avatar') {
+//             cd(null, './public/avatars');
+//         }
+//     },
+//     filename: function (req, file, cd) {
+//         cd(null, file.originalname);
+//     },
+// });
 
-// call function getIndex when client sends a request for '/' defined in routes.js
-// controller
-app.get('/', controller.getIndex);
+// var upload = multer({ storage: storage });
 
-app.post('/', controller.postLogIn);
+// Init Cookie and Body Parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.get('/checkUsername', controller.checkUsername);
+// Init Sessions
+app.use(
+    session({
+        key: 'user_sid', //user session id
+        secret: 'lifecouldbedream',
+        resave: false,
+        saveUninitialized: true,
+        store: database.sessionStore,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24, // 1 Day.
+        },
+    }),
+);
 
-app.get('/checkPassword', controller.checkPassword);
+app.use((req, res, next) => {
+    if (req.cookies.user_sid && !req.session.user) {
+        res.clearCookie('user_sid');
+    }
+    next();
+});
+
+
+// loginController
+app.get('/favicon.ico', loginController.getFavicon);
+app.get('/', loginController.getIndex);
+app.post('/', loginController.postLogIn);
+app.get('/checkUsername', loginController.checkUsername);
+app.get('/checkPassword', loginController.checkPassword);
 
 // signUpController
-
 app.get('/signup', signUpController.getSignUp);
-
 app.post('/signup', signUpController.postSignUp);
-
 app.get('/checkEmail', signUpController.checkEmail);
-
 app.get('/checkSignUpUsername', signUpController.checkSignUpUsername);
 
 // timelineController
-
 app.get('/HOME', timelineController.getTimeline);
-
 app.get('/createPost', timelineController.createPost);
-
 app.post('/uploadphoto', timelineController.uploadImage);
-
 app.get('/createPost', timelineController.createPost);
 
 // indivpostController
 app.get('/indivPost', indivpostController.getIndivPost);
-
 app.get('/createComment', indivpostController.createComment);
 
 // timelineController for Universities
 app.get('/ADMU', timelineController.getADMU);
-
 app.get('/DLSU', timelineController.getDLSU);
-
 app.get('/UP', timelineController.getUP);
-
 app.get('/UST', timelineController.getUST);
-
-// get status for each post
-
 app.get('/getStatus', timelineController.getStatus);
-
 app.get('/insertStatus', timelineController.insertStatus);
-
 app.get('/updateStatus', timelineController.updateStatus);
 
 // profileController
 app.get('/profile/:DisplayName', profileController.getUserProfile);
-
 app.get('/editprofile/:DisplayName', profileController.editProfile);
-
 app.get('/editprofile/:DisplayName', profileController.updateProfile);
 
 //app.get('/updateUpvote', timelineController.updateUpvote);
