4fabcfbea4ff3e150178f04b1a62d4932e8d01e9
iravillanuevaa <ira_villanueva@dlsu.edu.ph>
2021-02-24 20:12:31 +0800

    added ajax for upvote and downvote

--- controllers/postHelperController.js
+++ controllers/postHelperController.js
@@ -6,32 +6,31 @@ const db = require('../models/db.js');
 const postHelperController = {
     
     upvotePost: function(req, res) {
-        //var post_id = helper.sanitize(req.params.postId);
         var post_id = helper.sanitize(req.query.post_id);
         var id = helper.sanitize(req.session.user);
 
         console.log("hello");
         console.log(post_id);
-        // db.updateOne(Post,{_id: post_id, downvote: { $gt: 0} }, {$inc: {downvote: -1}}, function(post){
-        //     if(post){
-        //         //console.log(post);
-        //         db.updateOne(User,{_id: id}, {$pull: {postsDownVoted: post_id}}, function(user){
-        //             if(user){
-        //                 //console.log(user);
-        //                 db.updateOne(Post,{_id: post_id}, {$inc: {upvote: 1}}, function(post){
-        //                     if(post){
-        //                         //console.log(post);
-        //                         db.updateOne(User,{_id: id}, {$push: {postsUpVoted: post_id}}, function(user){
-        //                             if(user){
-        //                                 //console.log(user);
-        //                             }
-        //                         })
-        //                     }
-        //                 })      
-        //             }
-        //         })
-        //     }
-        // }) 
+        db.updateOne(Post,{_id: post_id, downvote: { $gt: 0} }, {$inc: {downvote: -1}}, function(post){
+            if(post){
+                //console.log(post);
+                db.updateOne(User,{_id: id}, {$pull: {postsDownVoted: post_id}}, function(user){
+                    if(user){
+                        //console.log(user);
+                        db.updateOne(Post,{_id: post_id}, {$inc: {upvote: 1}}, function(post){
+                            if(post){
+                                //console.log(post);
+                                db.updateOne(User,{_id: id}, {$push: {postsUpVoted: post_id}}, function(user){
+                                    if(user){
+                                        //console.log(user);
+                                    }
+                                })
+                            }
+                        })      
+                    }
+                })
+            }
+        }) 
     },
     
     unupvotePost: function(req, res) {
@@ -40,16 +39,16 @@ const postHelperController = {
 
         console.log("hello");
         console.log(post_id);
-        // db.updateOne(Post,{_id: post_id}, {$inc: {upvote: -1}}, function(post){
-        //     if(post){
-        //         //console.log(post);
-        //         db.updateOne(User,{_id: id}, {$pull: {postsUpVoted: post_id}}, function(user){
-        //             if(user){
-        //                 //console.log(user);
-        //             }
-        //         })
-        //     }
-        // })       
+        db.updateOne(Post,{_id: post_id}, {$inc: {upvote: -1}}, function(post){
+            if(post){
+                //console.log(post);
+                db.updateOne(User,{_id: id}, {$pull: {postsUpVoted: post_id}}, function(user){
+                    if(user){
+                        //console.log(user);
+                    }
+                })
+            }
+        })       
     },
 
     downvotePost: function(req, res) {
@@ -58,27 +57,27 @@ const postHelperController = {
 
         console.log("hello");
         console.log(post_id);
-        // db.updateOne(Post,{_id: post_id, upvote: {$gt: 0}}, {$inc: {upvote: -1}}, function(post){
-        //     if(post){
-        //         //console.log(post);
-        //         db.updateOne(User,{_id: id}, {$pull: {postsUpVoted: post_id}}, function(user){
-        //             if(user){
-        //                 //console.log(user);
-        //                 db.updateOne(Post,{_id: post_id}, {$inc: {downvote: 1}}, function(post){
-        //                     if(post){
-        //                         //console.log(post);
-        //                         db.updateOne(User,{_id: id}, {$push: {postsDownVoted: post_id}}, function(user){
-        //                             if(user){
-        //                                 //console.log(user);
-        //                             }
-        //                         })
+        db.updateOne(Post,{_id: post_id, upvote: {$gt: 0}}, {$inc: {upvote: -1}}, function(post){
+            if(post){
+                //console.log(post);
+                db.updateOne(User,{_id: id}, {$pull: {postsUpVoted: post_id}}, function(user){
+                    if(user){
+                        //console.log(user);
+                        db.updateOne(Post,{_id: post_id}, {$inc: {downvote: 1}}, function(post){
+                            if(post){
+                                //console.log(post);
+                                db.updateOne(User,{_id: id}, {$push: {postsDownVoted: post_id}}, function(user){
+                                    if(user){
+                                        //console.log(user);
+                                    }
+                                })
                                  
-        //                     }
-        //                 })       
-        //             }
-        //         })
-        //     }
-        // })      
+                            }
+                        })       
+                    }
+                })
+            }
+        })      
     },
 
     undownvotePost: function(req, res) {
@@ -87,16 +86,16 @@ const postHelperController = {
 
         console.log("hello");
         console.log(post_id);
-        // db.updateOne(Post,{_id: post_id}, {$inc: {downvote: -1}}, function(post){
-        //     if(post){
-        //         //console.log(post);
-        //         db.updateOne(User,{_id: id}, {$pull: {postsDownVoted: post_id}}, function(user){
-        //             if(user){
-        //                 //console.log(user);
-        //             }
-        //         })
-        //     }
-        // })
+        db.updateOne(Post,{_id: post_id}, {$inc: {downvote: -1}}, function(post){
+            if(post){
+                //console.log(post);
+                db.updateOne(User,{_id: id}, {$pull: {postsDownVoted: post_id}}, function(user){
+                    if(user){
+                        //console.log(user);
+                    }
+                })
+            }
+        })
     },
 
 
