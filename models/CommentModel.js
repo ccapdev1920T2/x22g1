54b11a31319b773d9a56eb1cb77a0a6dcb7e5242
iravillanuevaa <ira_villanueva@dlsu.edu.ph>
2021-02-26 12:38:28 +0800

    added comment schema

---
+++ models/CommentModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const commentSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Profile'
+    },
+    post: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post'
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    created: {
+        type: Date,
+        default: Date.now,
+    },
+})
+
+module.exports = mongoose.model('Comment', commentSchema);
