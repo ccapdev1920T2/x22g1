888df4849383d857acef8f7f6fa097d7781c17c0
victortulabot <44962471+victortulabot@users.noreply.github.com>
2021-05-14 20:30:03 +0800

    Update profile.js

--- public/js/profile.js
+++ public/js/profile.js
@@ -3,6 +3,22 @@
     // tablinks = document.getElementsByClassName("tablinks");
     // tablinks[0].className += " active";
 
+    $("#Recent_Post").ready(function(evt) {
+        var i, tabcontent, tablinks;
+
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        document.getElementById("RecentPost").style.display = "block";
+        evt.currentTarget.className += " active";
+    });
+
     $("#Recent_Post").click(function(evt) {
         var i, tabcontent, tablinks;
 
